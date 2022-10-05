2b50e88cb5b619898800961e0a6a901a55e69fc4
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-10-04 22:35:03 -0500

    create function, set timer

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const date = document.getElementById('date')
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
+setTimeout(getData, 2500)
+
 function getData() {
     header.innerHTML = '<img src="img/laptop.jpg" alt="laptop">'
     title.innerHTML = 'Lorem ipsum dolor sit amet.'
@@ -15,4 +17,9 @@ function getData() {
     profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="profile image"> '
     name.innerHTML = 'John Doe'
     date.innerHTML = 'Oct 08, 2020'
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+
+    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
 }
+
